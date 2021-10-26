773194d9c208917af8f256ff2a6ae04833675a93
vivi1 <viviendupont21@gmail.com>
2021-10-26 13:56:49 +0200

    Chargement

---
+++ Chargement.R
@@ -0,0 +1,9 @@
+# Libraries ---------------------------------------------------------------
+
+library(readr)
+
+
+# Chargement des données --------------------------------------------------
+
+SNP_Pass <- read_delim("VCF/output_gGVCF_sV_VF_SNP_PASS_VariantToTable.table", 
+           "\t", escape_double = FALSE, trim_ws = TRUE)
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# NGS_Projet
+# Projet de NGS
+
+Groupe : William Dartois & Vivien Dupont
+
+Travail sur le 7ème chromosome parmi deux échantillons de poules (maigres et dodues) afin d'identifier des variants ou génomes responsables des différences graisseuses.
