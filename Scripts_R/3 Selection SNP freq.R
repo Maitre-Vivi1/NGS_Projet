f63672ead63279bb8b7ec33cd5f508c68d19a828
vivi1 <viviendupont21@gmail.com>
2021-10-30 11:28:32 +0200

    petit conflit

--- Scripts_R/3 Selection SNP freq.R
+++ Scripts_R/3 Selection SNP freq.R
@@ -1,13 +1,13 @@
 # -------------------------------------------------------------------------
 
-SNP_long2 <- SNP_long[which(SNP_long$DP >= 80),]
+SNP_long3 <- SNP_long[which(SNP_long$DP >= 80),]
 # Qualité suffisante
 
-SNP_long2 <- SNP_long2[which(SNP_long2$maigre_00_gros_01_gros11 | SNP_long2$gros_00_maigre_01_maigre11),]
+SNP_long3 <- SNP_long3[which(SNP_long3$maigre_00_gros_01_gros11 | SNP_long3$gros_00_maigre_01_maigre11),]
 # Avec un gros contraste
 
 
-summary(as.factor(as.character(SNP_long2$Pos)))
+summary(as.factor(as.character(SNP_long3$Pos)))
 # 4 candidats
 
 select <- c(5432425, 5513019, 5514499, 5528011)
